a3d13119f7d6ad00b203e82adec1aae22e8b3acb
Chu Jae Yeong <123634960+chujaeyeong@users.noreply.github.com>
2023-06-26 17:46:19 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.02 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+   public int[] solution(String[] strlist) {
+		int[] answer = new int[strlist.length];
+		
+		for (int i = 0; i < answer.length; i++) {
+			answer[i] = strlist[i].length();
+		}
+		
+		
+		return answer;
+	}
+}
