6e046b1059975b8af1dc9038a865155f681762b7
Chu Jae Yeong <123634960+chujaeyeong@users.noreply.github.com>
2023-08-02 23:25:30 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 80.12 ms, Memory: 96.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -7,6 +7,11 @@ class Solution {
         String answer = "";
         
         // 1. Hash map을 만든다. (participant)
+        // 여기서, 동명이인이 있을 수도 있다는 전제가 있기 때문에, 
+        // HashMap의 getOrDefault(player, 0) 을 써서 player 라는 value가 있으면 가져오고,
+        // 아닐 경우 0을 Default로 지정해서 사용하겠다고 해야한다. value 자리에 1을 넣으면, 동명이인 예외 케이스를 
+        // 처리할 수가 없기 때문에 넣어주는 것~ 
+        
         HashMap<String, Integer> map = new HashMap<>();
         for (String player : participant) {
             map.put(player, map.getOrDefault(player, 0) + 1);
