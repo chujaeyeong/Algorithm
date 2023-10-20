446cb729540aaf2eb41d31fb0403d247079e6ca7
Chu Jae Yeong <chujy1224@gmail.com>
2023-10-21 00:45:49 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.58 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        
+        // 행렬의 곱셈은 왼쪽 행렬의 행 개수와 오른쪽 행렬의 열 개수를 갖게 된다
+        int[][] arr = new int[arr1.length][arr2[0].length];
+        
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++) {
+                arr[i][j] = 0;
+                for(int k=0; k<arr1[i].length; k++) {
+                    arr[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        
+        return arr;
+    }
+}
