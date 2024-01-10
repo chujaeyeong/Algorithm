df3fd85943ae91ec49d9d941d629b82fe88a3f46
Chu Jae Yeong <chujy1224@gmail.com>
2024-01-10 22:12:11 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 1.90 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        
+        // 음식을 순서대로 담을 StringBuilder 선언 
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++) {
+            // i번 음식을 2로 나눈 개수만큼 sb에 추가 
+            int count = food[i] / 2;
+            sb.append(String.valueOf(i).repeat(count));
+        }
+        
+        // sb에 추가된 음식에 0과 reverse된 문자열 더하기
+        String answer = sb + "0";
+        answer += sb.reverse();
+        
+        return answer;
+    }
+}
