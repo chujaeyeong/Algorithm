8121a602643ccf9ed9820143099bf6d10e2416d1
Chu Jae Yeong <chujy1224@gmail.com>
2024-01-24 19:05:45 +0900

    [Bronze I] Title: 약수, Time: 128 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        while (T --> 0) {
+            int N = Integer.parseInt(st.nextToken());
+            max = N > max ? N : max;
+            min = N < min ? N : min;
+        }
+        
+        System.out.println(max * min);
+    }
+}
