7b0be377047761b56fdbbf8cfddb26a36fc59713
Chu Jae Yeong <chujy1224@gmail.com>
2024-03-22 20:03:55 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 162.00 ms, Memory: 68 MB -BaekjoonHub

---
+++ 프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        
+        int answer = a + b;
+
+        System.out.println(a + " + " + b + " = " + answer);
+    }
+}
