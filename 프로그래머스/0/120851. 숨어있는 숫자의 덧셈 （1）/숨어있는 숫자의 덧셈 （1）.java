bb271fd1a3904f22c734f5aa535bdc4def54b642
Chu Jae Yeong <chujy1224@gmail.com>
2024-05-20 22:22:55 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.25 ms, Memory: 76.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String[] arr = my_string.replaceAll("[^0-9]", "").split("");
+        for (int i = 0; i < arr.length; i++) {
+            answer += Integer.parseInt(arr[i]);
+        }
+        
+        return answer;
+    }
+}
